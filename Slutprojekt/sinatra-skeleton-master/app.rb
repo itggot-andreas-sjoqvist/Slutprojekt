a07b2c9f6b079a1eab477f96c079220975bd4942
itggot-andreas-sjoqvist <andreas.sjoqvist@itggot.se>
2016-05-12 17:40:03 +0200

    Inlogg och registrering, dock inga cookies
    Login- och registreringslayout samt webbservern för detta gjord. Inga
    cookies dock, så sessionen sparas inte

--- Slutprojekt/sinatra-skeleton-master/app.rb
+++ Slutprojekt/sinatra-skeleton-master/app.rb
@@ -1,17 +1,54 @@
 class App < Sinatra::Base
   enable :sessions
 
-  get '/' do
-    @assignments = Assignment.all
-    @projects = Project.all
-    @users = User.all
-    erb :layout
+   before do
+     @user = User.get(session[:user])
   end
 
+  get '/' do
+    if session[:user_id]
+      erb :overview
+    else
+      redirect '/login'
+    end
+    end
+
   get '/login' do
-    @users = User.all
-    erb :syrgr
+    erb :login, layout: false
   end
+
+  get '/userrights' do
+    if session[:user_id]
+      erb :overview
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/user/login' do
+    user = User.first(email: params['email'])
+    if user && user.password == params['password']
+      session[:user_id] = user.id
+      redirect '/userrights'
+    end
+  end
+
+  post '/user/register' do
+    if params['password'] == params['confirm-password']
+      User.create(f_name: params['f_name'],
+                  l_name: params['l_name'],
+                         email: params['email'],
+                         password: params['password']
+                          )
+      redirect '/userrights'
+    end
+  end
+
+  post '/user/logout' do
+    session[:user_id] = nil
+    redirect '/'
+  end
+
 
 
 end
