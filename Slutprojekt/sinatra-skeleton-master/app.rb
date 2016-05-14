745ddfda16dc97bc00603b4e3cbfb5729cb4bd98
itggot-andreas-sjoqvist <andreas.sjoqvist@itggot.se>
2016-05-14 22:28:13 +0200

    Nu funkar faktiskt registering och login + lite design
    Sidomeny fungerar som den ska. Kan registera och logga in. Log out knapp
    ser bra ut.

--- Slutprojekt/sinatra-skeleton-master/app.rb
+++ Slutprojekt/sinatra-skeleton-master/app.rb
@@ -1,50 +1,60 @@
+
 class App < Sinatra::Base
   enable :sessions
 
-   before do
-     @user = User.get(session[:user])
-  end
+  before do
+       @user = User.get(session[:user])
+     end
 
   get '/' do
     if session[:user_id]
-      erb :overview
+        redirect '/home'
     else
       redirect '/login'
-    end
-    end
+      end
+  end
+
+  get '/home' do
+    @users = User.all
+    if session[:user_id]
+        @assignments = Assignment.all(user_id: @user_id)
+        @projects = Project.all(user_id: @user_id)
+        erb :overview
+    else
+      redirect '/'
+      end
+  end
 
   get '/login' do
     erb :login, layout: false
   end
 
-  get '/userrights' do
-    if session[:user_id]
-      erb :overview
-    else
-      redirect '/login'
-    end
-  end
 
   post '/user/login' do
     user = User.first(email: params['email'])
     if user && user.password == params['password']
       session[:user_id] = user.id
-      redirect '/userrights'
+      redirect '/home'
+    else
+      redirect back
     end
-  end
+    end
+
 
   post '/user/register' do
-    if params['password'] == params['confirm-password']
+    if params['password'] == params['confirm_password']
       User.create(f_name: params['f_name'],
                   l_name: params['l_name'],
                          email: params['email'],
-                         password: params['password']
+                         password: params['password'],
+                          role_id: 1
                           )
-      redirect '/userrights'
+      redirect '/home'
     end
+      redirect back
   end
 
-  post '/user/logout' do
+  get '/user/logout' do
     session[:user_id] = nil
     redirect '/'
   end
